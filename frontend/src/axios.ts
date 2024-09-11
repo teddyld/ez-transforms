b35031690332995d81324374200acd7096bdc456
Vincent <vincent.pham2@outlook.com>
2024-09-12 01:56:16 +1000

    Fixed AWS endpoint

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -4,7 +4,7 @@ import { toast } from "sonner";
 const DEFAULT_ERROR_TEXT =
   "An error occurred while processing your request. Please try again later.";
 
-axios.defaults.baseURL = "http://54.252.245.234:8080";
+axios.defaults.baseURL = "https://54.252.245.234:8080";
 axios.defaults.headers.put["Content-Type"] = "application/json";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.delete["Content-Type"] = "application/json";
